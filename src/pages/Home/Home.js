3641b0dfb0f43430c19fad4a3f0e27abb39137ea
AnastasiiaZastup <131608774+AnastasiiaZastup@users.noreply.github.com>
2024-01-02 16:11:32 +0200

    add part

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,32 @@
+import { Button } from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import {
+  selectIsLoggedin,
+  selectUserName,
+} from 'redux/authorization/authSelectors';
+import { FlexContainer, Titel } from 'reusable/Styles.styled';
+
+export default function HomePage() {
+  const isLoggedin = useSelector(selectIsLoggedin);
+  const userName = useSelector(selectUserName);
+
+  return isLoggedin ? (
+    <FlexContainer>
+      <Titel>Hello {userName}!</Titel>
+      <Button
+        variant="link"
+        colorScheme="purple"
+        width="200px"
+        marginLeft="auto"
+        marginRight="auto"
+      >
+        <Link to="/contacts">To my contacts</Link>
+      </Button>
+    </FlexContainer>
+  ) : (
+    <>
+      <Titel>Wellcome to Phonebook!</Titel>
+    </>
+  );
+}
